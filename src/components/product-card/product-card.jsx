9ddaa960cc959387bf6fab9a1b61ed66a2d75c54
asqarbekolimov <mr.coderx.pro@gmail.com>
2023-11-03 14:32:42 +0500

    feat: react query fecth data and product card

---
+++ src/components/product-card/product-card.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const ProductCard = ({ featured }) => {
+  console.log(featured);
+  return (
+    <>
+      <div className="group">
+        <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
+          <img
+            src={featured.image}
+            alt={featured.description}
+            className="h-full w-full object-cover object-center group-hover:opacity-75"
+          />
+        </div>
+        <h3 className="mt-4 text-sm text-gray-700">{featured.description}</h3>
+        <div className="flex items-center justify-between">
+          <p className="mt-1 text-lg font-medium text-gray-900">
+            ${featured.price}
+          </p>
+          <button className="bg-green-600 text-white py-1 px-3 rounded">
+            BUY
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ProductCard;
---
+++ src/services/config.js
@@ -0,0 +1,2 @@
+export const API_URL = "https://fortniteapi.io/v1/shop?lang=en";
+export const API_KEY = "9b114e07-dc2b0c08-b11ac306-96f0517c";
